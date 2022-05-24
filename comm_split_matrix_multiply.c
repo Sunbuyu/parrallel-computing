2a37d008ce169adb4d2e8b2635bf52266d0d19c4
SunJun <xiaopisung@foxmail.com>
2022-05-24 20:47:35 +0800

    split and collective communication

---
+++ comm_split_matrix_multiply.c
@@ -0,0 +1,7 @@
+/*  ***********************************************************************
+* Name : comm_split_matrix_multiply
+* Time : 2022.05.24
+* Author : SunMozhi
+* Version : 1.0
+* Copyright : GPL
+* Description : matrix A is divided to 4 row blocks, and a
--- matrix.cpp
+++
@@ -1,9 +0,0 @@
-//
-// Created by Xiaopi Zhu on 2022/3/4.
-//
-
-//#include "matrix.h"
-//using namespace std;
-//#include <vector>
-//
-//vector<int>
